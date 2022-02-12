b3178bc812847f60f493e557f32046581fe7cfb2
Nathan Kim <nathankim@Nathans-MacBook-Pro.local>
2022-02-12 14:51:53 -0500

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Hi world</h1>
     </div>
   );
 }

c4a2642a6aa953aad17634a253590766adb6a1f8
Nathan Kim <nathankim@Nathans-MacBook-Pro.local>
2022-02-12 16:02:32 -0500

    Added Grid: Frick you bob

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Main />
     </div>
   );
 }

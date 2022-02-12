7d67862ea7da6c3a17481d50f13beaa98c85d635
Daniel Ostapenko <danielostapenko@Daniels-MacBook-Air.local>
2022-02-12 17:48:08 -0500

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Main from '@mui/material/Utils';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/icons-material/Menu';
+import MenuItem from '@mui/icons-material/Menu';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import ClosedCaptionIcon from '@mui/icons-material/ClosedCaption';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Box sx={{ flexGrow: 8, display: "flex", justifyContent: "space-between" }}>
+      <AppBar position="static">
+        <Toolbar variant="dense" sx={{ justifyContent: "space-between" }}>
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuItem />
+          </IconButton>
+          <Typography align="center" variant="h6" color="black" component="div"/>
+            Wordle for nerds
+          <Typography align="right" variant="h6" color="black" component="div"/>
+          <GitHubIcon />
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 

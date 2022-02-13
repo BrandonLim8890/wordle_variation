a28acf9cb4fba2169f85e7027e5d28d3f8d4c0fe
Brandon Lim <brandonlim8890@gmail.com>
2022-02-13 13:47:27 -0500

    change color

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Main from './components/Main/Main';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Swaple';
+  }, []);
   return (
     <div className='App'>
       <h1 style={{ marginBottom: '5rem' }}>Swaple</h1>

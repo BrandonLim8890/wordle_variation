b732c6e2f28359890ebaaca5a00b37726d3ad744
meisbobzheng <94478583+meisbobzheng@users.noreply.github.com>
2022-02-12 23:44:07 -0500

    Changed the title

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Main from './components/Main/Main';
 function App() {
   return (
     <div className='App'>
-      <h1 style={{ marginBottom: '5rem' }}>wordle but hard</h1>
+      <h1 style={{ marginBottom: '5rem' }}>Swaple</h1>
       <Main />
     </div>
   );
